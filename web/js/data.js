612426e621e0dd66b441a7555caffc66bf62c8e2
boskoa <bosko.ap@gmail.com>
2024-04-10 23:02:39 +0200

    Adding sketching app

---
+++ web/js/data.js
@@ -0,0 +1,86 @@
+const inputContainer = document.getElementById("user-data");
+const studentButton = document.getElementById("student-button");
+const student = document.getElementById("student");
+const sketchContainer = document.getElementById("sketchContainer");
+const instructions = document.getElementById("instructions");
+const instructionsContainer = document.getElementById("instructions-container");
+const nextButton = document.getElementById("next");
+
+let index = 0;
+const labels = [
+  "car",
+  "fish",
+  "house",
+  "tree",
+  "bicycle",
+  "guitar",
+  "pencil",
+  "clock",
+];
+
+const data = {
+  student: null,
+  session: new Date().getTime(),
+  drawings: {},
+};
+
+function start() {
+  if (student.value === "") {
+    alert("Please, type your name");
+    return;
+  }
+
+  data.student = student.value;
+  inputContainer.style.visibility = "hidden";
+  sketchContainer.style.visibility = "visible";
+  instructionsContainer.style.visibility = "visible";
+
+  const label = labels[index];
+  instructions.innerText = `Please, draw a ${label}.`;
+}
+
+function next() {
+  console.log("SKEČ", sketchPad.paths);
+  if (!sketchPad.paths.length) {
+    alert("Draw this one, first.");
+    return;
+  }
+
+  if (index === labels.length - 1) {
+    sketchContainer.style.visibility = "hidden";
+    instructions.innerText = "Thank you!";
+    nextButton.innerText = "Save";
+    nextButton.onclick = save;
+    return;
+  }
+
+  const label = labels[index];
+  data.drawings[label] = sketchPad.paths;
+  sketchPad.reset();
+
+  index++;
+  const nextLabel = labels[index];
+  instructions.innerText = `Please, draw a ${nextLabel}.`;
+}
+
+function save() {
+  console.log("SAVE");
+  nextButton.style.display = "none";
+  instructions.innerText =
+    "Save your download alongside others in the dataset.";
+  const element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(JSON.stringify(data))
+  );
+
+  const fileName = data.session + ".json";
+  element.setAttribute("download", fileName);
+  element.style.display = "none";
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+
+studentButton.addEventListener("click", start);
+nextButton.addEventListener("click", next);
---
+++ web/js/sketchPad.js
@@ -0,0 +1,80 @@
+class SketchPad {
+  constructor(container, size = 400) {
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = size;
+    this.canvas.height = size;
+    this.canvas.style = `
+      background-color: white;
+      box-shadow: 0px 0px 10px 2px black;
+    `;
+
+    container.appendChild(this.canvas);
+
+    this.ctx = this.canvas.getContext("2d");
+    this.paths = [];
+    this.isDrawing = false;
+    this.undoBtn = document.getElementById("undo");
+
+    this.#addEventListeners();
+  }
+
+  reset() {
+    this.paths = [];
+    this.isDrawing = false;
+    this.#redraw();
+  }
+
+  #addEventListeners() {
+    this.canvas.onmousedown = (e) => {
+      const mouse = this.#getMouse(e);
+      this.paths.push([mouse]);
+      this.isDrawing = true;
+    };
+
+    this.canvas.onmousemove = (e) => {
+      if (this.isDrawing) {
+        const mouse = this.#getMouse(e);
+        this.paths[this.paths.length - 1].push(mouse);
+        this.#redraw();
+      }
+    };
+
+    document.onmouseup = () => {
+      this.isDrawing = false;
+    };
+
+    this.canvas.ontouchstart = (e) => {
+      this.canvas.onmousedown(e.touches[0]);
+    };
+
+    this.canvas.ontouchmove = (e) => {
+      this.canvas.onmousemove(e.touches[0]);
+    };
+
+    document.ontouchend = () => {
+      this.canvas.onmouseup();
+    };
+
+    this.undoBtn.onclick = () => {
+      if (this.paths.length) {
+        this.paths.pop();
+        this.#redraw();
+      }
+    };
+  }
+
+  #getMouse = (e) => {
+    const rect = this.canvas.getBoundingClientRect();
+    return [
+      Math.round(e.clientX - rect.left),
+      Math.round(e.clientY - rect.top),
+    ];
+  };
+
+  #redraw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    draw.paths(this.ctx, this.paths);
+  }
+}
+
+const sketchPad = new SketchPad(document.getElementById("sketchPadContainer"));
